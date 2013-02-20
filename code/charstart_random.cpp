9c4194784e6a79ff5800745c6b3c15e075b45b3b
noodles <rungumpth000@gmail.com>
2013-02-20 10:17:00 +0800

    简化函数

--- code/charstart_random.cpp
+++ code/charstart_random.cpp
@@ -5,40 +5,21 @@
 
 using namespace std;
 
-string generate(const char startWithChar)
+string genNumericRandom(int charLen)
 {
-    int charLen = 0;
-    bool bStartCharValid = false;
-    
-    if (startWithChar >= '0' && startWithChar <= '9')
-    {
-        charLen = 9 - 1;
-    	bStartCharValid = true;
-    }
-    else
-    {
-        charLen = 9;        
-    }
-    
     const int BUF_LEN  = 32;
-    char rndch[BUF_LEN];    // Passcode 长度是： [9, 16]
-
+    char rndch[BUF_LEN];   
+    
     while (1)
     {
         int i = 0;
         int fd = open("/dev/urandom", O_RDONLY);
+        
         if (fd != -1)
         {
             read(fd, rndch, charLen);
 
-            // 将从startWithChars对应的char数据中随机选一个作为首字符
-            if (bStartCharValid)
-            {
-                rndch[i] = startWithChar;
-                ++i;
-            }
-
-            for (; i < 9; ++i)
+            for (; i < charLen; ++i)
             {
                 rndch[i] = (char)(((rndch[i] & 0x0f) % 10) + '0');
             }
@@ -53,14 +34,15 @@ string generate(const char startWithChar)
         rndch[i] = '\0';
         break;
     }
+
     return string(rndch);
 }
 
 int main()
 {
-    cout << generate('7') << endl;
-    cout << generate('8') << endl;
-    cout << generate(0) << endl;
+    cout << genNumericRandom(6) << endl;
+    cout << genNumericRandom(7) << endl;
+    cout << genNumericRandom(7) << endl;
     
     return 0;
 }
