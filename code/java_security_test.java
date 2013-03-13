d44df2d46b84ec640543062eb0c9d0d8e2e7cc3b
noodles <rungumpth000@gmail.com>
2013-03-13 17:14:46 +0800

    添加有无security manager情况下的现象

--- code/java_security_test.java
+++ code/java_security_test.java
@@ -3,7 +3,10 @@ import java.io.FileNotFoundException;
 
 
 // 1. 直接 java SecurityTest 执行,看结果
+//    由于没有指定SecurityManager,所以所有权限都是开放的.
+//    (System.getSecurityManager() is null. There is no SecurityManager so everything is allowed and we were able to successfully open a file and read the system property.)
 // 2. 使用自定义权限执行: java -Djava.security.manager -Djava.security.policy=myapp.policy SecurityTest
+//    这里指定了SecurityManager和policy文件了.
 public class SecurityTest {
 
     public static void main(String[] args)
